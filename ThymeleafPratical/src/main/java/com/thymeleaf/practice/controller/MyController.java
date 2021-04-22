81d922d4fb98175ebdbd8078e6ec5d6bafbcf3b4
Ahire Hitesh <hbahire2014@gmail.com>
2021-04-22 22:33:58 +0530

    add thymeleaf project in spring boot

---
+++ ThymeleafPratical/src/main/java/com/thymeleaf/practice/controller/MyController.java
@@ -0,0 +1,22 @@
+package com.thymeleaf.practice.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyController {
+
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
+	public String about(Model model) {
+
+		model.addAttribute("name", "Hitesh Ahire");
+		model.addAttribute("date", new Date().toString());
+		System.out.println("about called...");
+		return "about";
+		// about.html
+	}
+}
