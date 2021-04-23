f605520d09ed2118dd8de664abb8ddad4894da26
Ahire Hitesh <hbahire2014@gmail.com>
2021-04-23 22:11:39 +0530

    code added

--- ThymeleafPratical/src/main/java/com/thymeleaf/practice/controller/MyController.java
+++ ThymeleafPratical/src/main/java/com/thymeleaf/practice/controller/MyController.java
@@ -1,9 +1,11 @@
 package com.thymeleaf.practice.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,5 +20,16 @@ public class MyController {
 		System.out.println("about called...");
 		return "about";
 		// about.html
+	}
+
+	// for loop example
+	@GetMapping("/example-loop")
+	public String iterater(Model model) {
+
+		// create list, set
+
+		List<String> names = List.of("Hitesh", "Ganesh", "Pradhunya", "Nikita");
+		model.addAttribute("names", names);
+		return "iterater";
 	}
 }
