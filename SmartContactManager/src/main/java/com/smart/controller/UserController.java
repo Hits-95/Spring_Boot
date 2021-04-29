aa85a4cca68aabdc3ca0c5dadff6509a5f6ffdb1
Ahire Hitesh <hbahire2014@gmail.com>
2021-04-29 21:08:14 +0530

    spring boot security has been added.

--- SmartContactManager/src/main/java/com/smart/controller/UserController.java
+++ SmartContactManager/src/main/java/com/smart/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.smart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
+	@RequestMapping(path = "/index")
+	public String dashBoard() {
+		return "user/dash_board";
+	}
 }
