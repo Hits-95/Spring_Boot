8dbc495f001637b971aab6f191016d94ed2b18e8
Ahire Hitesh <hbahire2014@gmail.com>
2021-05-01 23:19:40 +0530

    update sequery

--- SmartContactManager/src/main/java/com/smart/controller/UserController.java
+++ SmartContactManager/src/main/java/com/smart/controller/UserController.java
@@ -1,16 +1,33 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.smart.dao.UserDao;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private UserDao userDao;
+
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String dashBoard() {
-		System.out.println("user/index called");
+	public String dashBoard(Model model, Principal principal) {
+
+		// get username from security
+		String userName = principal.getName();
+		
+		System.out.println(userName);
+
+		// send data to view
+		model.addAttribute("title", "User-Dashboard");
+		model.addAttribute("user", this.userDao.getUserByUserName(userName));
 		return "user/dash_board";
 	}
 }
