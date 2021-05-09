4553c24cfc128988d2f6908c5e608f998105fe68
Ahire Hitesh <hbahire2014@gmail.com>
2021-05-09 23:04:52 +0530

    email sending code via java has been done

--- Learn_Mail/src/main/java/com/learn/mail/Learn_Mail/App.java
+++ Learn_Mail/src/main/java/com/learn/mail/Learn_Mail/App.java
@@ -1,14 +1,18 @@
 package com.learn.mail.Learn_Mail;
 
+import java.io.File;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.Part;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  * Hello world!
@@ -16,6 +20,76 @@ import javax.mail.internet.MimeMessage;
  */
 public class App {
 
+	// attachment method
+
+	private static void sendAttachmentMail(String message, String subject, String to, final String from) {
+		// variable for gmail host
+		String host = "smtp.gmail.com";
+
+		// get the system propertices
+		Properties properties = System.getProperties();
+		System.out.println(properties);
+
+		// setting imp info to properties object
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "587"); // 465
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+
+		// step : 1 to get the session object...
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(from, "HitsNiki@98");
+			}
+		});
+		session.setDebug(true);
+
+		// step : 2 compose the message [text, multimedia]
+		MimeMessage mimeMessage = new MimeMessage(session);
+		try {
+
+			// from mail
+			mimeMessage.setFrom(from);
+
+			// adding Recipient
+			mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to)); // you can also use internate
+																							// address array for sending
+																							// mai to multiple users
+			// adding subject to message
+			mimeMessage.setSubject(subject);
+
+			// attach file...
+			String path = "E:\\images\\PicsArt\\hit.jpg";
+
+			// set file, image
+			MimeMultipart mimeMultipart = new MimeMultipart();
+
+			// set text and file
+			MimeBodyPart textMime = new MimeBodyPart();
+			MimeBodyPart fileMime = new MimeBodyPart();
+
+			textMime.setText(message);
+			fileMime.attachFile(new File(path));
+
+			// set data into multipart
+			mimeMultipart.addBodyPart(textMime);
+			mimeMultipart.addBodyPart(fileMime);
+
+			mimeMessage.setContent(mimeMultipart);
+
+			// step : 3 send the message usding transpose class
+			Transport.send(mimeMessage);
+
+			System.out.println("Mail sended ");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// simply send text mail to specific single person
 	private static void sendMail(String message, String subject, String to, final String from) {
 
 		// variable for gmail host
@@ -57,13 +131,13 @@ public class App {
 			// adding text .. you can also use multipart like this
 			mimeMessage.setText(message);
 
-		//step :  3 send the message usding transpose class
+			// step : 3 send the message usding transpose class
 			Transport.send(mimeMessage);
-			
+
 			System.out.println("Mail sended ");
-			
+
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 
 	}
@@ -74,7 +148,9 @@ public class App {
 		String to = "hits.ahire98@gmail.com";
 		String from = "hbahire2014@gmail.com";
 
-		sendMail(message, subject, to, from);
+		// sendMail(message, subject, to, from);
+
+		sendAttachmentMail(message, subject, to, from);
 	}
 
 }
