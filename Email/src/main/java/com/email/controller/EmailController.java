211717e38c812fa668b1e2279462bee3eb9abf68
Ahire Hitesh <hbahire2014@gmail.com>
2021-05-10 09:01:44 +0530

    mail sending using spring boot has been done

--- Email/src/main/java/com/email/controller/EmailController.java
+++ Email/src/main/java/com/email/controller/EmailController.java
@@ -1,13 +1,36 @@
 package com.email.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.email.model.EmailRequest;
+import com.email.service.EmailService;
 
 @RestController
 public class EmailController {
 
+	@Autowired
+	private EmailService emailService;
+
 	@RequestMapping("/home")
 	public String home() {
 		return "this is email home controller";
+	}
+
+	// API to send mail
+	@PostMapping("/sendmail")
+	public ResponseEntity<?> sendMail(@RequestBody EmailRequest emailRequest) {
+
+		System.out.println(emailRequest);
+		if (this.emailService.sendMail(emailRequest.getMessage(), emailRequest.getSubject(), emailRequest.getTo()))
+			return ResponseEntity.ok("Mail was sended successfully.");
+		else
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Email wasn't sended.");
+
 	}
 }
--- Email/src/main/java/com/email/service/EmailService.java
+++ Email/src/main/java/com/email/service/EmailService.java
@@ -1,8 +1,75 @@
 package com.email.service;
 
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+	// simply send text mail to specific single person
+	public boolean sendMail(String message, String subject, String to) {
+
+		boolean flag = false;
+		String from = "hbahire2014@gmail.com";
+		// variable for gmail host
+		String host = "smtp.gmail.com";
+
+		// get the system propertices
+		Properties properties = System.getProperties();
+		System.out.println(properties);
+
+		// setting imp info to properties object
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "587"); // 465
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+
+		// step : 1 to get the session object...
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(from, "*********");
+			}
+		});
+		session.setDebug(true);
+
+		// step : 2 compose the message [text, multimedia]
+		MimeMessage mimeMessage = new MimeMessage(session);
+		try {
+
+			// from mail
+			mimeMessage.setFrom(from);
+
+			// adding Recipient
+			mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to)); // you can also use internate
+																							// address array for sending
+																							// mai to multiple users
+			// adding subject to message
+			mimeMessage.setSubject(subject);
+
+			// adding text .. you can also use multipart like this
+			mimeMessage.setText(message);
+
+			// step : 3 send the message usding transpose class
+			Transport.send(mimeMessage);
+
+			System.out.println("Mail sended ");
+
+			flag = true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+
+	}
 
 }
